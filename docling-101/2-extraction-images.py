69c6f54fa786744dd00d721d411e162eee294aa5
Rodrigo <razevedo.contato@gmail.com>
2026-02-10 08:00:21 -0300

    PDF image extraction

---
+++ docling-101/2-extraction-images.py
@@ -0,0 +1,24 @@
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import PdfPipelineOptions
+from docling_core.types.doc import PictureItem
+import os
+
+pipeline_options = PdfPipelineOptions()
+pipeline_options.images_scale = 2.0
+pipeline_options.generate_picture_images = True
+
+converter = DocumentConverter(
+    format_options={InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)}
+)
+
+result = converter.convert("./2408.09869v5.pdf")
+
+os.makedirs("images", exist_ok=True)
+
+picture_counter = 0
+for element, _level in result.document.iterate_items():
+    if isinstance(element, PictureItem):
+        picture_counter += 1
+        with open(f"images/picture_{picture_counter}.png", "wb") as fp:
+            element.get_image(result.document).save(fp, "PNG")
